2b58160d50cbcbdf0ab6a0cd9857268762b3e265
yogirajzala369 <yogirajzala31@gmail.com>
2023-05-21 15:02:28 -0700

    feat-nautilus: Creating Calculator component and displaying digits, math operations, C and DEL buttons

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,26 @@
+const Calculator = () => {
+  return (
+    <div>
+      <div>Calculated result will show up here</div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>0</button>
+      <button>+</button>
+      <button>-</button>
+      <button>*</button>
+      <button>/</button>
+      <button>C</button>
+      <button>DEL</button>
+      <button>Calculate</button>
+    </div>
+  );
+};
+
+export default Calculator;
