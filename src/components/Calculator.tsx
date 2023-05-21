b6246e5276071d679e614b68ac9474879d70fb7e
yogirajzala369 <yogirajzala31@gmail.com>
2023-05-21 15:49:46 -0700

    feat-nautilus: Making calculator beautiful
    - Adding confetti on initial page load
    - Adding chakraUI styles for making UI responsive and better

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,5 +1,32 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useState, useMemo } from "react";
+import {
+  Box,
+  Button,
+  Center,
+  Grid,
+  Heading,
+  Input,
+  InputProps,
+} from "@chakra-ui/react";
 import { useCalculatorStore } from "../store";
+import Confetti from "react-confetti";
+
+const CustomInput = (props: InputProps) => {
+  return (
+    <Input
+      {...props}
+      textAlign="right"
+      mb={4}
+      size="lg"
+      variant="filled"
+      borderRadius="md"
+      placeholder="0"
+      readOnly
+      _placeholder={{ color: "gray.400" }}
+      _focus={{ borderColor: "teal.400" }}
+    />
+  );
+};
 
 const Calculator = () => {
   const {
@@ -10,6 +37,23 @@ const Calculator = () => {
     appendToExpression,
     evaluateExpression,
   } = useCalculatorStore();
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [loadingDone, setLoadingDone] = useState(false);
+
+  useEffect(() => {
+    setShowConfetti(true);
+
+    // Simulating loading completion after 5 seconds
+    const loadingTimeout = setTimeout(() => {
+      setLoadingDone(true);
+    }, 5000);
+
+    return () => clearTimeout(loadingTimeout); // Clear the loading timeout if the component unmounts
+  }, []);
+
+  useEffect(() => {
+    if (loadingDone) setShowConfetti(false); // Stop displaying confetti when loading is done
+  }, [loadingDone]);
 
   const handleButtonClick = useCallback(
     (value: string) => appendToExpression(value),
@@ -42,7 +86,7 @@ const Calculator = () => {
 
   const renderButton = useCallback(
     (num: string) => (
-      <button
+      <Button
         key={num}
         onClick={() =>
           num === "C"
@@ -51,21 +95,49 @@ const Calculator = () => {
             ? removeFromExpression()
             : handleButtonClick(num)
         }
+        size="lg"
       >
         {num}
-      </button>
+      </Button>
     ),
     [clear, handleButtonClick, removeFromExpression]
   );
 
   return (
-    <div>
-      <input aria-label="Expression" value={expression} />
-      <input aria-label="Result" value={result} />
-      {numbers.map(renderButton)}
-      <button onClick={evaluateExpression}>Calculate</button>
-      <button>Calculate</button>
-    </div>
+    <Center h="100vh">
+      {showConfetti && <Confetti />}
+      <Box p={4} maxW="400px" m="auto">
+        <Heading color="teal" as="h1" size="lg" mb={4}>
+          CALCULATOR
+        </Heading>
+        <Box
+          borderWidth={1}
+          borderRadius="md"
+          p={4}
+          bg="gray.50"
+          boxShadow="0 12px 48px rgb(26 176 176)"
+        >
+          <CustomInput aria-label="Expression" value={expression} />
+          <CustomInput
+            aria-label="Result"
+            value={result}
+            color="white"
+            backgroundColor="black"
+          />
+          <Grid templateColumns="repeat(4, 1fr)" gap={2}>
+            {numbers.map(renderButton)}
+            <Button
+              onClick={evaluateExpression}
+              size="lg"
+              w="100%"
+              colorScheme="teal"
+            >
+              Calculate
+            </Button>
+          </Grid>
+        </Box>
+      </Box>
+    </Center>
   );
 };
 
